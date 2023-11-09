6025172e995dc7b60f897527384c44786bdf76c0
j-koziel <jonathankoziel1@gmail.com>
2023-11-09 21:54:43 +0000

    feat: create form for searching recipes

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,17 @@ export default async function Index() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <h1>Welcome to recipe explorer</h1>
       </div>
+      <form className="animate-in flex flex-col gap-4 items-center w-screen">
+        <input
+          type="text"
+          placeholder="Pizza? Or maybe some lasagna? Make your pick..."
+          className="w-1/2 text-center rounded-md h-7 bg-inherit border"
+          autoFocus
+        />
+        <button className="bg-green-700 w-fit p-2 rounded-md" type="submit">
+          Search for a recipe
+        </button>
+      </form>
     </div>
   );
 }
